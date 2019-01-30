bbad1c9359c24e60017d2d4577d5ce2f2865f1c2
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-30 03:24:51 +0000

    logged http to the console for 4 different stages

---
+++ async.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+	
+	var http = new XMLHttpRequest();
+	
+	http.onreadystatechange = function() {
+		console.log(http);
+	};
+	
+	http.open("GET", "data/tweets.json", true);
+	http.send();
+	// console.log(http);
+};
+
+// Ready States
+
+// 0- request not initialized
+// 1- request has been set up
+// 2- request has been sent
+// 3- request is in process
+// 4- request is complete
