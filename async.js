5e8ebcb04d0aa7d06791ddbd715365384f21751f
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-31 01:13:05 +0000

    too many callback functions from ajax requests

--- async.js
+++ async.js
@@ -1,28 +1,71 @@
 window.onload = function(){
 	
-	var http = new XMLHttpRequest();
-	
-	http.onreadystatechange = function() {
-		if(http.readyState == 4 && http.status == 200){
-			// console.log(JSON.parse(http.response));
-		}
-	};
-	
-	http.open("GET", "data/tweets.json", true);
-	http.send();
-
-	//jquery method
-	
-	$.get("data/tweets.json", function(data){
-	console.log(data);	
+	$.ajax({
+	type: "GET",
+	url: "data/tweets.json",
+	success: function(data){
+		console.log(data);
+		
+	$.ajax({
+	type: "GET",
+	url: "data/friends.json",
+	success: function(data){
+		console.log(data);
+		
+	$.ajax({
+	type: "GET",
+	url: "data/videos.json",
+	success: function(data){
+		console.log(data);
+		
+	},
+	error: function(jqXHR, textStatus, error){
+		console.log(error);
+	}
 	});
-	console.log("test")
+	},
+	error: function(jqXHR, textStatus, error){
+		console.log(error);
+	}
+	});
+	},
+	error: function(jqXHR, textStatus, error){
+		console.log(error);
+	}
+	});
+	
 };
 
-// Ready States
 
-// 0- request not initialized
-// 1- request has been set up
-// 2- request has been sent
-// 3- request is in process
-// 4- request is complete
+
+
+
+
+// window.onload = function(){
+	
+// 	var http = new XMLHttpRequest();
+	
+// 	http.onreadystatechange = function() {
+// 		if(http.readyState == 4 && http.status == 200){
+// 			// console.log(JSON.parse(http.response));
+// 		}
+// 	};
+	
+// 	http.open("GET", "data/tweets.json", true);
+// 	http.send();
+
+// 	//jquery method
+	
+// 	$.get("data/tweets.json", function(data){
+// 	console.log(data);	
+// 	});
+// 	console.log("test")
+// };
+
+// // Ready States
+
+// // 0- request not initialized
+// // 1- request has been set up
+// // 2- request has been sent
+// // 3- request is in process
+// // 4- request is complete
